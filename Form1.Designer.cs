ffeca8f0fd90d50cda74742e310d94235b1b1902
Ruslan <abishev2012@gmail.com>
2023-06-15 22:22:55 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace MyAllocator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sizeTextBox = new System.Windows.Forms.TextBox();
+            this.AllocButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sizeTextBox
+            // 
+            this.sizeTextBox.Location = new System.Drawing.Point(52, 145);
+            this.sizeTextBox.Name = "sizeTextBox";
+            this.sizeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.sizeTextBox.TabIndex = 0;
+            // 
+            // AllocButton
+            // 
+            this.AllocButton.Location = new System.Drawing.Point(64, 206);
+            this.AllocButton.Name = "AllocButton";
+            this.AllocButton.Size = new System.Drawing.Size(75, 23);
+            this.AllocButton.TabIndex = 1;
+            this.AllocButton.Text = "Alloc";
+            this.AllocButton.UseVisualStyleBackColor = true;
+            this.AllocButton.Click += new System.EventHandler(this.AllocButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AllocButton);
+            this.Controls.Add(this.sizeTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sizeTextBox;
+        private System.Windows.Forms.Button AllocButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Forms;
+using static MyAllocator.Form1;
+
+namespace MyAllocator
+{
+    public partial class Form1 : Form
+    {
+        private MyAllocator _allocator;
+        private IntPtr _allocatedPtr;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _allocator = new MyAllocator(1024);
+        }
+
+
+
+        public class MyAllocator
+        {
+            private int _pageSize;
+            private byte[][] _chunks = null;
+            private int _curChunkIdx = -1;
+            private int _curByteIdx = 0;
+
+
+            public MyAllocator(int pageSize)
+            {
+                _pageSize = pageSize;
+            }
+
+            public IntPtr Alloc(int size)
+            {
+                if (_chunks == null || _curByteIdx + size > _pageSize)
+                {
+                    _chunks = new byte[10][];
+                    for (int i = 0; i < 10; i++)
+                    {
+                        _chunks[i] = new byte[_pageSize];
+                    }
+                    _curChunkIdx = 0;
+                    _curByteIdx = 0;
+                }
+
+                IntPtr ptr = new IntPtr(BitConverter.ToInt32(_chunks[_curChunkIdx], 0) + _curByteIdx);
+                _curByteIdx += size;
+                if (_curByteIdx >= _pageSize)
+                {
+                    _curChunkIdx++;
+                    _curByteIdx = 0;
+                }
+                return ptr;
+            }
+
+
+
+        }
+
+        private void AllocButton_Click(object sender, EventArgs e)
+        {
+            int size;
+            if (!int.TryParse(sizeTextBox.Text, out size))
+            {
+                MessageBox.Show("Недопустимый размер");
+                return;
+            }
+
+            _allocatedPtr = _allocator.Alloc(size);
+            MessageBox.Show($"Память, выделенная по адресу {_allocatedPtr.ToInt32()}");
+        }
+    }
+    
+}
